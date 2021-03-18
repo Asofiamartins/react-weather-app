426d0ae5eca8beeaa784619da6874644c474c4c1
Asofiamartins <77688504+Asofiamartins@users.noreply.github.com>
2021-03-18 19:27:24 +0000

    Delete files

--- src/Forecast.js
+++ src/Forecast.js
@@ -6,34 +6,39 @@ import "./Forecast.css"
 
 export default function Forecast(){
     return (
-    <div className="forecastInfo">
-      <div className="row">
-            <div className="col">
-              <p>Monday</p>
-             <img src="img/01.svg" alt="Monstera Deliciosa"/>
-            <p>19º | 10º</p>
-            </div>
-            <div className="col">
-              <p>Monday</p>
-             <img src="img/01.svg" alt="Monstera Deliciosa"/>
-            <p>19º | 10º</p>
-            </div>
-            <div className="col">
-              <p>Monday</p>
-             <img src="img/01.svg" alt="Monstera Deliciosa"/>
-            <p><span className="maxTemp">19º</span> |<span className="minTemp">10º</span></p>
-            </div>
-            <div className="col">
-              <p>Monday</p>
-             <img src="img/01.svg" alt="Monstera Deliciosa"/>
-            <p>19º | 10º</p>
-            </div>
-            <div className="col">
-              <p>Monday</p>
-             <img src="img/01.svg" alt="Monstera Deliciosa"/>
-            <p>19º | 10º</p>
-            </div>
-            
+      <div>
+        <div className="weatherButton">
+          <button className="dailyButton">Daily Forecast</button>
+          <button className="weeklyButton">Weekly Forecast</button>
+        </div>
+        <div className="forecastInfo">
+            <div className="row">
+              <div className="col">
+                <p>Monday</p>
+                 <img src="img/01.svg" alt="Monstera Deliciosa"/>
+                <p>19º | 10º</p>
+              </div>
+              <div className="col">
+                <p>Monday</p>
+                <img src="img/01.svg" alt="Monstera Deliciosa"/>
+                <p>19º | 10º</p>
+              </div>
+              <div className="col">
+                  <p>Monday</p>
+                  <img src="img/01.svg" alt="Monstera Deliciosa"/>
+                  <p><span className="maxTemp">19º</span> |<span className="minTemp">10º</span></p>
+                </div>
+              <div className="col">
+                  <p>Monday</p>
+                  <img src="img/01.svg" alt="Monstera Deliciosa"/>
+                  <p>19º | 10º</p>
+              </div>
+              <div className="col">
+                  <p>Monday</p>
+                  <img src="img/01.svg" alt="Monstera Deliciosa"/>
+                  <p>19º | 10º</p>
+              </div>
+            </div>   
       </div>  
     </div>);
 }
